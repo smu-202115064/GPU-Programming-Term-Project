61d157cf69795b32cac785d36d9525fc848da534
Hepheir <hepheir@gmail.com>
2024-03-20 16:10:39 +0900

    refactor: shader path 수정

--- src/shaders_class.cpp
+++ src/shaders_class.cpp
@@ -48,7 +48,7 @@ int main()
 
     // build and compile our shader program
     // ------------------------------------
-    Shader ourShader("3.3.shader.vs", "3.3.shader.fs"); // you can name your shader files however you like
+    Shader ourShader("shader/3.3.shader.vs", "shader/3.3.shader.fs"); // you can name your shader files however you like
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
