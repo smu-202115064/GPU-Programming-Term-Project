4b63c78a639fc6a90a3a25648e9f79017b6248f6
Hepheir <hepheir@gmail.com>
2024-05-28 04:52:08 +0900

    feat: 타임스톤에 알파블렌드 + 텍스쳐 합성

--- shader/timestone.fs
+++ shader/timestone.fs
@@ -4,12 +4,19 @@ out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
+
+const vec3 green = vec3(0.2, 1.0, 0.2);
+const vec3 black = vec3(0.0, 0.0, 0.0);
 
 void main()
 {
     vec3 diffuse = texture(texture_diffuse1, TexCoords).rgb;
-    vec3 ambient = vec3(0.2, 1.0, 0.2);
-    vec3 color = mix(diffuse, ambient, 0.2);
-    float alpha = 0.72;
-    FragColor = vec4(color, alpha);
+    vec3 specular = texture(texture_specular1, TexCoords).rgb;
+    vec3 color = mix(diffuse, green, 0.32);
+    if (specular.g > 0.2)
+    {
+        color = mix(color, black, 0.2);
+    }
+    FragColor = vec4(color, 0.72);
 }
